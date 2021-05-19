7a7cb70493bfdfdab2cdd008fc1ea65b68e4ffc7
Zdeal <jedimastershake@gmail.com>
2021-05-18 22:04:56 -0400

    js file corrected working on text

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ password generator
 # Table of Contents
 -[Purpose](#Purpose)
 
--[Corrections](#Corrections)
+-[Criteria](#Criteria)
 
 -[Notes](#Notes)
 
@@ -17,20 +17,29 @@ password generator
 
 
 # Purpose
+The purpose of this exercise is to create a working javascript file to make a password generator work. The html and css file did not need any alteration.
 
 
-# Corrections
-1. 
-2. 
-3. 
-4. 
-5. 
+# Criteria
+1. When generate is clicked, the user is presented with a series of prompts to include:
+    -How many characters (between 8-128)?
+    -Do you want numbers?
+    -Do you want upper case letters?
+    -Do you want lower case letters?
+    -Do you want special characters?
+
+2. When prompts are answered at least one character type should be present for each selected.
+
+3. When password is generated it should match the selected prompts.
+
+4. The password is then displayed
 
 
 # Notes
+This task was extrememly difficult at first. In my head everything was jumbling up but it became clearer the further into it I got. Creating a drawing and taking notes on paper really helped with this but I am working so that I don't have to use this method because it is time consuming.
 
 # Credits
-Matierial and ideas borrowed from Trilogy Education services along with teachings from Anthony Cooper and Sasha Peterson.
+Matierial and ideas borrowed from Trilogy Education services along with teachings from Anthony Cooper and Sasha Peterson. Youtube videos on password generators also utilized.
 
 
 # Screenshot        
--- script.js
+++ script.js
@@ -8,10 +8,10 @@ var confirmSpecials;
 
 //list of possible choices for the function
 
-var smalls = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var biggies = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var characters = ["!", "@", "#", "$", "%", "^", "&", "*", "+", "-"]
+smalls = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+biggies = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+specials = ["!", "@", "#", "$", "%", "^", "&", "*", "+", "-"]
 
 var pick;
 
@@ -19,9 +19,11 @@ var pick;
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//adding what happens when button is clicked
+
 generateBtn.addEventListener("click", function () {
   pw = generatePassword();
-  domcument.getElementById("password").placeholder = pw;
+  document.getElementById("password").placeholder = pw;
 });
 
 //^^check this
@@ -29,61 +31,67 @@ generateBtn.addEventListener("click", function () {
 // begin user selections
 function generatePassword() {
 
-select = parseInt(prompt("Please select a number between 8 and 128"));
+  select = parseInt(prompt("Please select a number between 8 and 128"));
 
-if (!select) {
+  if (!select) {
 
-  alert("Plese select a number between 8 and 128");
+    alert("Plese select a number between 8 and 128");
 
-}
+  }
 
-else if (select < 8 || select > 128) {
+  else if (select < 8 && select > 128) {
 
-  select = parseInt(prompt("PLEASE slect a number between 8 and 128"));
-  //parseint funciton parses a string argument and returns the integer of the specified base
+    select = parseInt(prompt("PLEASE slect a number between 8 and 128"));
+    //parseint funciton parses a string argument and returns the integer of the specified base
 
-} 
+  }
 
-else {
- confirmBiggies = confirm("Do you want upper case letters?");
- confirmSmalls = confirm("Do you want lower case letters?");
- confirmNumbers = confirm("Do you want numbers?");
- confirmSpecials = confirm("Do you want special characters?");
-};
-}
+  else {
+    confirmBiggies = confirm("Do you want upper case letters?");
+    confirmSmalls = confirm("Do you want lower case letters?");
+    confirmNumbers = confirm("Do you want numbers?");
+    confirmSpecials = confirm("Do you want special characters?");
+  };
+
+
 
 //user doesn't select any option
 
 if (!confirmSmalls && !confirmBiggies && !confirmNumbers && !confirmSpecials) {
-  select = alert("Please select something to build your password");
+
+  select = alert("Please hit generate to start");
+
 }
+
+//^^ location does this need to be at the end?
 //user picks all options
 
 else if (confirmSmalls && confirmBiggies && confirmNumbers && confirmSpecials) {
-  select = character.concat(biggies, smalls, specials, numbers);
+
+  select = biggies.concat(smalls, specials, numbers);
 
   //user picks three************
 
   //biggie numbers and specials
 } else if (confirmBiggies && confirmNumbers && confirmSpecials) {
 
-  pick = biggies.concat(numbers, specials);
+  select = biggies.concat(numbers, specials);
 }
 //biggie numbers and smalls
 else if (confirmBiggies && confirmNumbers && confirmSmalls) {
 
-  pick = biggies.concat(numbers, smalls);
+  select = biggies.concat(numbers, smalls);
 }
 //biggie smalls and specials
 
 else if (confirmBiggies && confirmSmalls && confirmSpecials) {
 
-  pick = biggies.concat(smalls, specials);
+  select = biggies.concat(smalls, specials);
 }
 //smalls numbers specials
 else if (confirmSmalls && confirmNumbers && confirmSpecials) {
 
-  pick = smalls.concat(numbers, specials);
+  select = smalls.concat(numbers, specials);
 
 }
 
@@ -105,11 +113,11 @@ else if (confirmBiggies && confirmSmalls) {
 
   select = smalls.concat(numbers);
 
-}  else if (confirmSmalls && confirmSpecials) {
+} else if (confirmSmalls && confirmSpecials) {
 
-    select = smalls.concat(specials)
+  select = smalls.concat(specials)
 
-} else if (confirmNumbers && confirmSpecials) { 
+} else if (confirmNumbers && confirmSpecials) {
 
   select = numbers.concat(specials)
 }
@@ -129,32 +137,37 @@ else if (confirmBiggies) {
 
 } else if (confirmSpecials) {
 
-  select - specials;
+  select = specials;
 
 };
 
 //placed holder for length
 
-var length = []
+var password = []
 
 //random selection of variables
-
+//loop
 for (var i = 0; i < select; i++) {
   var pickSelect = select[Math.floor(Math.random() * select.length)];
   password.push(pickSelect);
-
-  var pw = password.join("");
-  userInput(pw);
-  ; pw;
 }
 
-funciton, userInput(pw); {
+
+
+var pw = password.join("");
+userInput(pw);
+return pw;
+}
+
+
+function userInput(pw) {
+
   document.getElementById("password").textContent = pw;
 
 }
 
-var copy = document.querySelector("copy");
-copy.addEventListener("click", function () {
-  copyPassword();
+var generate = document.querySelector("#generate");
+generate.addEventListener("click", function () {
+  generatePassword();
 
 })
