984158ad5ce0cdd38c97150f417c50f6c76a8af0
Zdeal <jedimastershake@gmail.com>
2021-09-17 18:12:49 -0400

    debugging with tutor

--- script.js
+++ script.js
@@ -1,172 +1,111 @@
-// user selections
-
-var select;
-var confirmSmalls;
-var confirmBiggies;
-var confirmNumbers;
-var confirmSpecials;
-
 //list of possible choices for the function
-
-smalls = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-biggies = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-specials = ["!", "@", "#", "$", "%", "^", "&", "*", "+", "-"]
-
-var pick;
-
-
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+var smalls = "abcdefghijklmnopqrstuvwxyz".split("");
+var biggies = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var numbers = "0123456789".split("");
+var specials = "!@#$%^&*+-".split("");
 
 //adding what happens when button is clicked
-
-generateBtn.addEventListener("click", function () {
-  pw = generatePassword();
-  document.getElementById("password").placeholder = pw;
-});
 
 //^^check this
 
 // begin user selections
 function generatePassword() {
+  var select = parseInt(prompt("Please select a number between 8 and 128"));
 
-  select = parseInt(prompt("Please select a number between 8 and 128"));
-
-  if (!select) {
-
-    alert("Plese select a number between 8 and 128");
-
-  }
-
-  else if (select < 8 && select > 128) {
-
-    select = parseInt(prompt("PLEASE slect a number between 8 and 128"));
+  if (select < 8 && select > 128) {
+    return prompt("PLEASE slect a number between 8 and 128");
     //parseint funciton parses a string argument and returns the integer of the specified base
-
+  } else {
+    var confirmBiggies = confirm("Do you want upper case letters?");
+    var confirmSmalls = confirm("Do you want lower case letters?");
+    var confirmNumbers = confirm("Do you want numbers?");
+    var confirmSpecials = confirm("Do you want special characters?");
   }
 
-  else {
-    confirmBiggies = confirm("Do you want upper case letters?");
-    confirmSmalls = confirm("Do you want lower case letters?");
-    confirmNumbers = confirm("Do you want numbers?");
-    confirmSpecials = confirm("Do you want special characters?");
-  };
+  //user doesn't select any option
 
+  var choicesArr = [];
 
+  if (
+    !confirmSmalls &&
+    !confirmBiggies &&
+    !confirmNumbers &&
+    !confirmSpecials
+  ) {
+    return alert("Please select an option");
+  }
 
-//user doesn't select any option
+  //^^ location does this need to be at the end?
+  //user picks all options
+  else if (
+    confirmSmalls &&
+    confirmBiggies &&
+    confirmNumbers &&
+    confirmSpecials
+  ) {
+    choicesArr = biggies.concat(smalls, specials, numbers);
 
-if (!confirmSmalls && !confirmBiggies && !confirmNumbers && !confirmSpecials) {
+    //user picks three************
 
-  select = alert("Please hit generate to start");
+    //biggie numbers and specials
+  } else if (confirmBiggies && confirmNumbers && confirmSpecials) {
+    choicesArr = biggies.concat(numbers, specials);
+  }
+  //biggie numbers and smalls
+  else if (confirmBiggies && confirmNumbers && confirmSmalls) {
+    choicesArr = biggies.concat(numbers, smalls);
+  }
+  //biggie smalls and specials
+  else if (confirmBiggies && confirmSmalls && confirmSpecials) {
+    choicesArr = biggies.concat(smalls, specials);
+  }
+  //smalls numbers specials
+  else if (confirmSmalls && confirmNumbers && confirmSpecials) {
+    choicesArr = smalls.concat(numbers, specials);
+  }
 
-}
+  //user picks 2 *************
+  else if (confirmBiggies && confirmSmalls) {
+    choicesArr = biggies.concat(smalls);
+  } else if (confirmBiggies && confirmNumbers) {
+    choicesArr = biggies.concat(numbers);
+  } else if (confirmBiggies && confirmSpecials) {
+    choicesArr = biggies.concat(specials);
+  } else if (confirmSmalls && confirmNumbers) {
+    choicesArr = smalls.concat(numbers);
+  } else if (confirmSmalls && confirmSpecials) {
+    choicesArr = smalls.concat(specials);
+  } else if (confirmNumbers && confirmSpecials) {
+    choicesArr = numbers.concat(specials);
+  }
 
-//^^ location does this need to be at the end?
-//user picks all options
+  //user picks 1 *************
+  else if (confirmBiggies) {
+    choicesArr = biggies;
+  } else if (confirmSmalls) {
+    choicesArr = smalls;
+  } else if (confirmNumbers) {
+    choicesArr = numbers;
+  } else if (confirmSpecials) {
+    choicesArr = specials;
+  }
 
-else if (confirmSmalls && confirmBiggies && confirmNumbers && confirmSpecials) {
+  //placed holder for length
+  var password = [];
 
-  select = biggies.concat(smalls, specials, numbers);
+  //random choicesArrion of variables
+  //loop
+  for (var i = 0; i < select; i++) {
+    var pickSelect = choicesArr[Math.floor(Math.random() * choicesArr.length)];
+    password.push(pickSelect);
+  }
 
-  //user picks three************
+  var pw = password.join("");
 
-  //biggie numbers and specials
-} else if (confirmBiggies && confirmNumbers && confirmSpecials) {
-
-  select = biggies.concat(numbers, specials);
-}
-//biggie numbers and smalls
-else if (confirmBiggies && confirmNumbers && confirmSmalls) {
-
-  select = biggies.concat(numbers, smalls);
-}
-//biggie smalls and specials
-
-else if (confirmBiggies && confirmSmalls && confirmSpecials) {
-
-  select = biggies.concat(smalls, specials);
-}
-//smalls numbers specials
-else if (confirmSmalls && confirmNumbers && confirmSpecials) {
-
-  select = smalls.concat(numbers, specials);
-
-}
-
-//user picks 2 *************
-
-else if (confirmBiggies && confirmSmalls) {
-
-  select = biggies.concat(smalls);
-
-} else if (confirmBiggies && confirmNumbers) {
-
-  select = biggies.concat(numbers);
-
-} else if (confirmBiggies && confirmSpecials) {
-
-  select = biggies.concat(specials);
-
-} else if (confirmSmalls && confirmNumbers) {
-
-  select = smalls.concat(numbers);
-
-} else if (confirmSmalls && confirmSpecials) {
-
-  select = smalls.concat(specials)
-
-} else if (confirmNumbers && confirmSpecials) {
-
-  select = numbers.concat(specials)
-}
-
-//user picks 1 *************
-else if (confirmBiggies) {
-
-  select = biggies;
-
-} else if (confirmSmalls) {
-
-  select = smalls;
-
-} else if (confirmNumbers) {
-
-  select = numbers;
-
-} else if (confirmSpecials) {
-
-  select = specials;
-
-};
-
-//placed holder for length
-
-var password = []
-
-//random selection of variables
-//loop
-for (var i = 0; i < select; i++) {
-  var pickSelect = select[Math.floor(Math.random() * select.length)];
-  password.push(pickSelect);
-}
-
-
-
-var pw = password.join("");
-userInput(pw);
-return pw;
-}
-
-
-function userInput(pw) {
-  document.getElementById("password").textContent = pw;
-
+  return pw;
 }
 
 var generate = document.querySelector("#generate");
 generate.addEventListener("click", function () {
-  generatePassword();
-
-})
+  document.getElementById("password").textContent = generatePassword();
+});
